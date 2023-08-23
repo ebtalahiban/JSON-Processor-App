e14b070839cd1b96330c673938706cb87289352c
Erica Talahiban <ebtalahiban@up.edu.ph>
2023-08-23 12:16:39 +0800

    Send POST request from frontend to backend

--- client/src/components/UserInput.js
+++ client/src/components/UserInput.js
@@ -1,23 +1,45 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './UserInput.css';
 
 const UserInput = () => {
   const [userText, setUserText] = useState('');
+  const [backendResponse, setBackendResponse] = useState('');
 
   const handleTextChange = (event) => {
     setUserText(event.target.value);
   };
 
+  const sendQueryToBackend = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/query', { userText });
+      setBackendResponse(response.data.userText); 
+    } catch (error) {
+      console.error('Error querying backend:', error);
+    }
+  };
+
   return (
     <div className="user-input">
-        <h2>User Editable Text</h2>
-        <textarea
+      <h2>User Editable Text</h2>
+      <textarea
         rows="4"
         cols="50"
         className="user-input-textarea"
         value={userText}
         onChange={handleTextChange}
-        />
+      />
+      <button className="user-input-button" onClick={sendQueryToBackend}>
+        Query Backend
+      </button>
+      <div className="backend-response">
+        {backendResponse && (
+          <div>
+            <h3>Backend Response:</h3>
+            <p>{backendResponse}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/api/query', (req, res) => {
   const userText = req.body.userText;
